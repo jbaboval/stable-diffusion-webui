e899ea4301936bbd77eb8acb0b3250c9d3eee03f
John Baboval <baboval.github@spineless.org>
2023-01-27 08:21:39 -0500

    Update codeformers

--- webui.py
+++ webui.py
@@ -115,7 +115,10 @@ def check_versions():
     if shared.cmd_opts.skip_version_check:
         return
 
-    expected_torch_version = "2.0.0"
+    if devices.device.type == 'xpu':
+        expected_torch_version = '1.13.0'
+    else:
+        expected_torch_version = "2.0.0"
 
     if version.parse(torch.__version__) < version.parse(expected_torch_version):
         errors.print_error_explanation(f"""
